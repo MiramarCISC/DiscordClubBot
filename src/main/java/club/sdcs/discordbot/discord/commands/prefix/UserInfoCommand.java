ca720c3c3e104fbf78113ef18bb1a43148d9531f
lenhekin <lenhekin@gmail.com>
2024-07-11 11:43:53 -0700

    UserInfoCommand -New command only accessible to officers -Checks for user info by typing command "!user info [user tag]" -Returns user's real name if they have already done the registration process, otherwise bot tells that user has not completed the process -Returns a message when user does not exist

---
+++ src/main/java/club/sdcs/discordbot/discord/commands/prefix/UserInfoCommand.java
@@ -0,0 +1,93 @@
+package club.sdcs.discordbot.discord.commands.prefix;
+
+import club.sdcs.discordbot.service.UserService;
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
+@Component
+public class UserInfoCommand implements PrefixCommand {
+
+    private final UserService userService;
+    private final long officerId = 1261017068917227712L;
+
+    public UserInfoCommand(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public String getName() {
+        return "!user info";
+    }
+
+    @Override
+    public Mono<Void> handle(Message message) {
+
+        return message.getAuthorAsMember()
+                .flatMap(member -> isOfficer(member)
+                        .flatMap(isOfficer -> {
+                            if (!isOfficer) {
+                                return message.getChannel()
+                                        .flatMap(channel -> channel.createMessage("You must be an officer to use this command."))
+                                        .then();
+                            }
+
+                            String[] content = message.getContent().split(" ");
+                            String userTag = String.join(" ", Arrays.copyOfRange(content, 2, content.length));;
+
+                            return message.getGuild()
+                                    .flatMap(guild -> findMemberByTag(guild, message, userTag))
+                                    .flatMap(member1 -> {
+                                        long userId = member1.getId().asLong();
+                                        String realName;
+
+                                        if (userService.getUserByDiscordId(userId) != null) {
+                                            realName = userService.getUserByDiscordId(userId).getFullName();
+                                        } else {
+                                            realName = null;
+                                            return message.getChannel()
+                                                    .flatMap(channel -> channel.createMessage("This user has not completed the registration process."))
+                                                    .then();
+                                        }
+
+                                        return message.getChannel()
+                                                .flatMap(channel -> channel.createMessage("The real name of this user is: " + realName))
+                                                .then();
+
+                                    });
+
+                        }));
+
+    }
+
+    /**
+     * checks if user who types command is officer
+     * @param member takes in member
+     * @return if member is officer or not
+     */
+    private Mono<Boolean> isOfficer(Member member) {
+        return Mono.just(member.getRoleIds()
+                .stream()
+                .anyMatch(roleId -> roleId.asLong() == officerId));
+    } //end isOfficer()
+
+    /**
+     * finds member by the user tag
+     * @param guild takes in guild of user
+     * @param userTag takes in user tag
+     * @return if it finds a match
+     */
+    private Mono<Member> findMemberByTag(Guild guild, Message message, String userTag) {
+        return guild.requestMembers()
+                .filter(member -> member.getTag().equalsIgnoreCase(userTag))
+                .next()
+                .switchIfEmpty(message.getChannel()
+                        .flatMap(channel -> channel.createMessage("User does not exist. Make sure you use the user's tag and not display name."))
+                        .then(Mono.empty()));
+    }//end findMemberByTag()
+
+}
